0f833b9f6d0aec6890f430404d1b23faf9e81c92
songze <songze@qutoutiao.net>
2020-08-05 19:18:26 +0800

    fix: 完善命令行

--- packages/zdxb-cli/bin/zdxb-cli.js
+++ packages/zdxb-cli/bin/zdxb-cli.js
@@ -5,23 +5,6 @@ const program = require('commander');
 const minimist = require('minimist');
 const chalk = require('chalk');
 
-program.version(require('../package').version).usage('<command> [options]');
-
-program
-  .command('create <app-name>')
-  .description('根据项目模板创建一个新项目')
-  .action((name, cmd) => {
-    const options = cleanArgs(cmd);
-    if (minimist(process.argv.slice(3))._.length > 1) {
-      console.log(
-        chalk.yellow(
-          '\n Info: 参数超过一个时，只有第一个参数会被作为项目创建的目录，其余参数都将被忽略'
-        )
-      );
-    }
-    require('../lib/create').default(name, options);
-  });
-
 function camelize(str) {
   return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''));
 }
@@ -39,4 +22,38 @@ function cleanArgs(cmd) {
   return args;
 }
 
+program.version(require('../package').version).usage('<command> [options]');
+
+program
+  .command('create <app-name>')
+  .description('根据项目模板创建一个新项目')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd);
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(
+        chalk.yellow(
+          '\n Info: 参数超过一个时，只有第一个参数会被作为项目创建的目录，其余参数都将被忽略'
+        )
+      );
+    }
+    require('../lib/create').default(name, options);
+  });
+
+program.arguments('<cmd>').action((cmd) => {
+  program.outputHelp();
+  console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
+});
+
+program.on('--help', () => {
+  console.log();
+  console.log(
+    `  Run ${chalk.cyan(`zdxb-cli <command> --help`)} for detailed usage of given command.`
+  );
+  console.log();
+});
+
 program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
